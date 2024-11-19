12750c6fbbd095dc494357e2f5b9a3d18c93ae8e
Didar Auyesbay <66788568+ded-r@users.noreply.github.com>
2024-11-19 18:10:11 +0500

    Update main.py

--- main.py
+++ main.py
@@ -12,18 +12,22 @@ def index():
 
 @app.route('/apply-filter', methods=['POST'])
 def apply_filter():
-    if 'image' not in request.files:
-        return {"error": "No image uploaded"}, 400
+    try:
+        if 'image' not in request.files:
+            return {"error": "No image uploaded"}, 400
+    
+        image_file = request.files['image']
+        image = Image.open(image_file)
+        filtered_image = image.filter(ImageFilter.BLUR)
+    
+        output = io.BytesIO()
+        filtered_image.save(output, format='JPEG')
+        output.seek(0)
 
-    image_file = request.files['image']
-    image = Image.open(image_file)
-    filtered_image = image.filter(ImageFilter.BLUR)
-
-    output = io.BytesIO()
-    filtered_image.save(output, format='JPEG')
-    output.seek(0)
-
-    return send_file(output, mimetype='image/jpeg', as_attachment=False)
-
+        return send_file(output, mimetype='image/jpeg', as_attachment=False
+                         
+      except Exception as e:
+        return jsonify(error=str(e)), 500
+          
 if __name__ == "__main__":
     app.run(debug=True)
